28fa1bca38fa38c32aef82473696c5d4a20c8fe5
Paul Mumbi <127586250+PaulChola96@users.noreply.github.com>
2023-03-14 16:48:24 +0200

    Add files via upload

---
+++ FIM/FIM.ps1
@@ -0,0 +1,117 @@
+﻿Set-Location C:/Users/mumbi/OneDrive/Desktop/SKILLS/PROJECTS/Computer_InfoSec_1996/FIM
+Add-Type -AssemblyName system.speech
+$voice= New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+
+$FileVoiceAndText = "PLEASE SELECT WHAT YOU WOULD LIKE TO BEGIN WITH"
+$CreateBaselineText ="A) Create and Update baseline file?"
+$StartMonitorCheck = "B) Monitor files with saved Baseline?"
+
+Write-Host "********************************************************"
+Write-Host $FileVoiceAndText
+$voice.Speak($FileVoiceAndText)
+Write-Host "##############################################"
+Write-Host $CreateBaselineText
+Write-Host $StartMonitorCheck
+Write-Host "##############################################"
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+Write-Host "*****************************************"
+
+
+Function Calculate-File-Hash($filepath){
+$filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+return $filehash
+}
+
+Function Erase-Baseline-If-Already-Exists(){
+  $baselineExists = Test-Path -Path .\baseline.txt
+
+  if($baselineExists){
+    
+    # Delete it 
+      Remove-Item -Path .\baseline.txt
+  
+  }
+ 
+}
+
+if ($response -eq "A".ToUpper()) {
+# Delete baseline.txt if it already exists
+  Erase-Baseline-If-Already-Exists 
+
+# Calculate Hash from the target files and store in baseline.txt
+# Collect all files in the target folder
+$files = Get-ChildItem -Path .\Files
+
+# for file, calculate the bash, and write to baseline.txt
+ foreach ($f in $files){
+   $hash = Calculate-File-Hash $f.FullName
+   "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+    $voice.Speak(" Baseline file has been created and updated!")
+ 
+   }
+
+}
+
+elseif ($response -eq "B".ToUpper()) {
+
+    $fileHashDictionary = @{}
+
+# Load file|hash from baseline.txt and store them in a dictionary
+    $filePathsAndHashes = Get-Content -Path .\baseline.txt
+    
+    foreach ($f in $filePathsAndHashes) {
+             $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+    }
+     $fileHashDictionary.keys
+
+
+# Begin continously monitoring files with saved Baseline
+
+   While($true){
+   start-Sleep -Seconds 1
+
+    $files = Get-ChildItem -Path .\Files
+   
+ # For each file, calculate the hash, and write to baseline.txt
+        foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.FullName
+            #"$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+     
+ # Notify if a new file has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null) {
+                # A new file has been created!
+                Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+                $voice.Speak("($key.Path) has been created!!!!")
+            }
+  # Notify if a new file has been changed
+                if ($fileHashDictionary[$hash.Path] -eq $hash.Hash) {
+                    # The file has not changed
+                    Write-Host "$($hash.Path) has not changed!!!" -ForegroundColor Blue
+                    $voice.Speak("($key.Path) has not changed!!!!")
+                }
+
+               else {
+  # File file has been compromised!, notify the user
+                    Write-Host "$($hash.Path) has been altered or changed!!!" -ForegroundColor Yellow
+                     $voice.Speak("($key.Path) has been altered or changed!!!!")
+                }      
+
+             }
+
+      foreach ($key in $fileHashDictionary.Keys) {
+                $baselineFileStillExists = Test-Path -Path $key
+            if (-Not $baselineFileStillExists) {
+  # One of the baseline files must have been deleted, notify the user
+                Write-Host "$($key) has been deleted!" -ForegroundColor Red -BackgroundColor Gray
+                 $voice.Speak("($key.Path) has been deleted!")
+
+
+
+          }
+
+  
+       }  
+
+    } 
+
+}
---
+++ IP PINGGER/IP-ping.ps1
@@ -0,0 +1,46 @@
+﻿Set-Location "C:\Users\mumbi\OneDrive\Desktop\SKILLS\PROJECTS\Computer_InfoSec_1996\IP PINGGER"
+
+Add-Type -AssemblyName System.Speech
+
+$Voice = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+$ipAddresses = "192.168.0.1", "8.8.8.8", "127.1" # Replace with the IP addresses you want to ping, separated by commas
+$nslookups = "www.google.com", "www.microsoft.com" # Replace with the URLs you want to lookup, separated by commas
+$outputFile = "ping-results.txt" # Replace with the filename and path where you want to save the results
+$nsoutputFile = "nslookup-results.txt"  # Replace with the filename and path where you want to save the results  
+
+foreach ($ipAddress in $ipAddresses) {
+    # Ping the IP address and save the output to a variable
+    $pingOutput = Test-Connection -ComputerName $ipAddress -Count 1
+    
+    # Save the output to a file
+    $pingOutput | Out-File $outputFile -Append
+}
+
+foreach($nslookup in $nslookups) {
+    # lookup IP address and save the output to a variable
+    $nsLookupOut = Resolve-DnsName -Name $nslookup -Type A -Server 8.8.8.8
+
+    # Save the output to a file
+    $nsLookupOut | Out-File $nsoutputFile -Append
+}
+
+Function Created-File {
+    $pinglist = Test-Path -Path .\ping-results.txt
+
+    if ($pinglist) {
+        Write-Host "Ping results file has been created"
+        $Voice.Speak("Ping results file has been created and updated")
+    }
+}
+
+Function Created-File-Nslookup {
+    $nslist = Test-Path -Path .\nslookup-results.txt
+
+    if ($nslist) {
+        Write-Host "NSLookup results file has been created"
+        $Voice.Speak("NSLookup results file has been created and updated")
+    }
+}
+
+Created-File
+Created-File-Nslookup
